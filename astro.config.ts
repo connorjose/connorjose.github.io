c60c79448174aa8692b602bce925e2e7e88181cc
connor b <31991894+connorjose@users.noreply.github.com>
2025-10-21 13:01:20 +1000

    Switch to mdx to embed custom components

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro'
+import Icons from 'unplugin-icons/vite'
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [
+            Icons({ 
+                compiler: 'astro',
+            })
+        ]
+    },
     integrations: [UnoCSS({
-        injectReset: true
-    })],
+    injectReset: true
+    }), mdx()],
 });
